6cd171157b290b31a2a80eb253a9b88019d42deb
Saleh Ibne Omar <salehibneomar@gmail.com>
2021-12-18 22:23:01 +0600

    Added custom 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>404 - Page Not Found</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ asset('images/settings/logo.png') }}">
+
+    <!-- page css -->
+
+    <!-- Core css -->
+    <link href="{{ asset('backend/assets/css/app.min.css') }}" rel="stylesheet">
+
+</head>
+
+<body>
+    <div class="app">
+        <div class="container-fluid">
+            <div class="d-flex full-height p-v-50 flex-column justify-content-between">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-md-12">
+                            <div class="p-v-100 text-center">
+                                <h1 class="font-weight-semibold display-1 text-primary lh-1-2">404</h1>
+                                <h2 class="font-weight-light font-size-30">Whoops! Looks like you got lost</h2>
+                                <p class="lead m-b-30">We couldnt find what you were looking for.</p>
+                                <button onclick="history.back()" class="btn btn-primary btn-tone">GO BACK</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
